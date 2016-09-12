0fc053c148ec2845c115ca4b9437837b606c8beb
pfilippo <petr.filippov@gmail.com>
2016-09-12 11:04:51 +0300

    for tests

--- week1/app.js
+++ week1/app.js
@@ -1,7 +1,24 @@
 (function (){
 	'use strict';
 	angular.module("LunchCheck", [])
-	.controller("lcC",function($scope){
+	.controller("LunchCheckController", LunchCheckController);
 
-	});
+	LunchCheckController.$inject = ['$scope'];
+
+	function LunchCheckController($scope) {
+		$scope.message = "";
+		$scope.dishes = "";
+		$scope.Check = function(){
+			var ld = $scope.dishes.trim();
+			if (ld == "") {
+				$scope.message = "Please enter data first";
+				$scope.textStyle = {color:'red'};
+			}
+			else  {
+				if  (ld.split(',').length <= 3) $scope.message = "Enjoy!";
+				else $scope.message = "Too much!";
+				$scope.textStyle = {color:'green'};
+			}
+		}
+	}
 })();
