e12b8c7dacbddcc32f9140a19bcc5b1e21a4e9d0
Peter Filippov <petr.filippov@gmail.com>
2016-11-03 16:53:44 +0300

    week 5, fixed URL paths

--- week5/src/common/common.module.js
+++ week5/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'http://pfilippo.herokuapp.com')
+.constant('ApiPath', 'https://pfilippo.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
